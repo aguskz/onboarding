461be658a2d7e858d9adc5486e27b982a1aba5f1
AgusKz <agustin.fernandez@kzsoftworks.com>
2023-01-20 10:05:42 -0300

    setup page

--- pages/setup/index.tsx
+++ pages/setup/index.tsx
@@ -1,14 +1,20 @@
 import { useRef } from "react";
 import { motion, useScroll } from "framer-motion";
+import Head from "next/head";
+import { tools } from "../../constants";
 
 export default function Setup() {
   const ref = useRef(null);
   const { scrollXProgress } = useScroll({ container: ref });
 
-  return (
-    <>
-    <div className="bg-white w-10 h-10"></div>
+  return ( 
+  <div className="flex min-h-screen flex-col items-center justify-center py-2">
+  <Head>
+    <title>Create Next App</title>
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
       <svg id="progress" width="100" height="100" viewBox="0 0 100 100">
+        <circle cx="50" cy="50" r="30" pathLength="1" className="bg" />
         <motion.circle
           cx="50"
           cy="50"
@@ -18,19 +24,22 @@ export default function Setup() {
           style={{ pathLength: scrollXProgress }}
         />
       </svg>
-      <ul ref={ref}>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-    </>
+        <p className="text-2xl mx-60">
+            By now, you got a high-performance device enough to satisfy all the technical requirements the project demands. Below the list of software you need to install:
+        </p>
+        <ul ref={ref} className="w-8/12 max-h-80">
+            {tools.map(({name, link}) => 
+            <li className="flex bg-black flex-col justify-between rounded-md border hover:border-yellow-300 focus:border-yellow-300">
+                <div>
+                    <p className="text-white text-xl font-semibold p-3">{name}</p>
+                </div>
+                {link ? <div className="flex justify-end">
+                    <a className="flex rounded-md bg-yellow-300 font-mono text-lg px-2 mb-2 mr-2" target="_blank" href={link}>
+                    <p className="text-black m-0 p-1 tracking-normal">Download</p>
+                    </a>
+                </div> : null}
+            </li>)}
+        </ul>
+    </div>
   );
 }
