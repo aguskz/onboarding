3ea264b07b7f3f9afc685b5a9fd1601075f89927
AgusKz <agustin.fernandez@kzsoftworks.com>
2023-01-20 10:56:09 -0300

    removed unused code

--- components/Parallax/index.tsx
+++ components/Parallax/index.tsx
@@ -1,5 +1,3 @@
-// Photos from https://citizenofnowhe.re/lines-of-the-city
-
 import { useRef } from "react";
 import {
   motion,
@@ -39,40 +37,6 @@ function Image({ name, image, title, subtitle, link }: { id: number, name: strin
       </motion.h2>
       
     </section>
-  );
-}
-
-function Test() {
-  const ref = useRef(null);
-  const { scrollXProgress } = useScroll({ container: ref });
-
-  return (
-    <>
-      <svg id="progress" width="100" height="100" viewBox="0 0 100 100">
-        <circle cx="50" cy="50" r="30" pathLength="1" className="bg" />
-        <motion.circle
-          cx="50"
-          cy="50"
-          r="30"
-          pathLength="1"
-          className="indicator"
-          style={{ pathLength: scrollXProgress }}
-        />
-      </svg>
-      <ul ref={ref}>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-    </>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,17 +12,10 @@ const Home: NextPage = () => {
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold mb-10">
           Welcome partner!
-          {/* <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a> */}
         </h1>
         <img src="kaizen.png" className='flex max-h-20 w-auto h-auto pl-5'/>
         <p className="text-2xl">
         The whole crew is happy to have you on board!
-          {/* Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code> */}
         </p>
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
           <a
@@ -36,7 +29,7 @@ const Home: NextPage = () => {
           </a>
 
           <a
-            href="/setup"
+            href="/access"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:border-yellow-300 focus:border-yellow-300"
           >
             <h3 className="text-2xl font-bold">Additional access &rarr;</h3>
@@ -67,18 +60,6 @@ const Home: NextPage = () => {
         </div>
 
       </main>
-
-      {/* <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer> */}
     </div>
   )
 }
