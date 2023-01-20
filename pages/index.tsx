090f9179b1587384b38cff37808ff8b5ff798d88
AgusKz <agustin.fernandez@kzsoftworks.com>
2023-01-20 14:01:40 -0300

    home motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,26 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
+import * as React from 'react'
+import { motion } from 'framer-motion'
+
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+}
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+}
 
 const Home: NextPage = () => {
   return (
@@ -10,10 +31,16 @@ const Home: NextPage = () => {
         <p className="text-2xl">
           The whole crew is happy to have you on board!
         </p>
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
+        <motion.div
+          className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full"
+          variants={container}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.a
             href="/setup"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:border-yellow-300 focus:border-yellow-300"
+            variants={item}
           >
             <h3 className="text-2xl font-bold">
               Installation and Setup &rarr;
@@ -21,40 +48,43 @@ const Home: NextPage = () => {
             <p className="mt-4 text-xl">
               The list of software you need to install.
             </p>
-          </a>
+          </motion.a>
 
-          <a
+          <motion.a
             href="/access"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:border-yellow-300 focus:border-yellow-300"
+            variants={item}
           >
             <h3 className="text-2xl font-bold">Additional access &rarr;</h3>
             <p className="mt-4 text-xl">
               All the documentation related to the project is consolidaded
               inside Notion.
             </p>
-          </a>
+          </motion.a>
 
-          <a
+          <motion.a
             href="/repos"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:border-yellow-300 focus:border-yellow-300"
+            variants={item}
           >
             <h3 className="text-2xl font-bold">Repositories &rarr;</h3>
             <p className="mt-4 text-xl">
               Apps currently under construction that conform the whole project.
             </p>
-          </a>
+          </motion.a>
 
-          <a
+          <motion.a
             href="/diagram.png"
             download
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:border-yellow-300 focus:border-yellow-300"
+            variants={item}
           >
             <h3 className="text-2xl font-bold">Overview &rarr;</h3>
             <p className="mt-4 text-xl">
               Diagram that outlines microservices and how they are related.
             </p>
-          </a>
-        </div>
+          </motion.a>
+        </motion.div>
       </main>
     </div>
   )
