c3721456906456dd1b2c1a629d52934c8aabe001
AgusKz <agustin.fernandez@kzsoftworks.com>
2023-01-20 13:03:14 -0300

    local head removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,6 @@ import Head from 'next/head'
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Kz Reframe Onboarding</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold mb-10">Welcome partner!</h1>
         <img src="kaizen.png" className="flex max-h-20 w-auto h-auto pl-5" />
