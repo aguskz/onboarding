5968f1e539afb88d855cb8f7e0ee3570154587fd
AgusKz <agustin.fernandez@kzsoftworks.com>
2023-01-20 13:02:50 -0300

    global head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,18 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import '../components/Parallax/styles.css'
 import '../pages/setup/styles.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Kz Reframe Onboarding</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
