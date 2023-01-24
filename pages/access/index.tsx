e4a28f6aa80fe21da2ab9681b82a082d1f6e9b3f
AgusKz <agustin.fernandez@kzsoftworks.com>
2023-01-24 14:33:16 -0300

    font size adjusted

--- pages/access/index.tsx
+++ pages/access/index.tsx
@@ -4,13 +4,13 @@ import Carousel from '../../components/Carousel'
 export default function Setup() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <p className="text-2xl mx-60">
+      <p className="text-xl mx-60">
         All the documentation related to the project is consolidaded inside
         Notion. Please, consider Notion as the source of truth for requirements,
         technical specifications, wireframes, shared documents and more.
       </p>
       <Carousel list={access} linkName={'See more'} />
-      <p className="text-2xl text-white mx-60 mt-10">
+      <p className="text-xl text-white mx-60 mt-10">
         We are more than happy to request access on your behalf and we normally
         do it in that way. But just in case, if access is not granted, please
         let us know. We will connect with the client right away.
@@ -19,7 +19,7 @@ export default function Setup() {
         {additionalAccess.map(({ name, link, linkName }) => (
           <div className="flex flex-col justify-between rounded-md bg-zinc-900">
             <div>
-              <p className="text-white text-xl p-3 text-opacity-60">{name}</p>
+              <p className="text-white p-3 text-opacity-60">{name}</p>
             </div>
             {link ? (
               <div className="flex justify-end pr-2 pb-2">
--- pages/setup/index.tsx
+++ pages/setup/index.tsx
@@ -4,7 +4,7 @@ import Carousel from '../../components/Carousel'
 export default function Setup() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <p className="text-2xl mx-60">
+      <p className="text-xl mx-60">
         By now, you got a high-performance device enough to satisfy all the
         technical requirements the project demands. Below the list of software
         you need to install:
