c04e9b9527661e05e23dfd02ea317a83b3cf04d8
AgusKz <agustin.fernandez@kzsoftworks.com>
2023-01-24 14:25:32 -0300

    build fix

--- pages/repos/index.tsx
+++ pages/repos/index.tsx
@@ -1,6 +1,24 @@
 import { Parallax } from '../../components'
+import { repos } from '../../constants'
 
-const Repos = () => {
-  return <Parallax />
+export async function getServerSideProps() {
+  const front = await fetch(
+    `${process.env.NEXT_GITLAB_LINK}/${process.env.NEXT_GITLAB_FRONTEND_ID}?private_token=${process.env.NEXT_GITLAB_TOKEN}`,
+  )
+  const back = await fetch(
+    `${process.env.NEXT_GITLAB_LINK}/${process.env.NEXT_GITLAB_BACKEND_ID}?private_token=${process.env.NEXT_GITLAB_TOKEN}`,
+  )
+  const [{ projects: projectsFront }, { projects: projectsBack }] =
+    await Promise.all([front.json(), back.json()])
+  return {
+    props: {
+      projects: [...projectsFront, ...projectsBack],
+    },
+  }
+}
+
+const Repos = (/*{ projects }: { projects?: any }*/) => {
+  // const { name, web_url, avatar_url } = projects
+  return <Parallax repos={repos} />
 }
 export default Repos
