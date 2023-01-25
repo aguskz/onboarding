82e934da9eb368ea141ec1e01867e4387eae4839
AgusKz <agustin.fernandez@kzsoftworks.com>
2023-01-25 12:22:12 -0300

    gitlab error handled

--- pages/repos/index.tsx
+++ pages/repos/index.tsx
@@ -2,18 +2,25 @@ import { Parallax } from '../../components'
 import { repos } from '../../constants'
 
 export async function getServerSideProps() {
-  const front = await fetch(
-    `${process.env.NEXT_GITLAB_LINK}/${process.env.NEXT_GITLAB_FRONTEND_ID}?private_token=${process.env.NEXT_GITLAB_TOKEN}`,
-  )
-  const back = await fetch(
-    `${process.env.NEXT_GITLAB_LINK}/${process.env.NEXT_GITLAB_BACKEND_ID}?private_token=${process.env.NEXT_GITLAB_TOKEN}`,
-  )
-  const [{ projects: projectsFront }, { projects: projectsBack }] =
-    await Promise.all([front.json(), back.json()])
-  return {
-    props: {
-      projects: [...projectsFront, ...projectsBack],
-    },
+  let projects: Array<any> = []
+  try {
+    const front = await fetch(
+      `${process.env.NEXT_PUBLIC_GITLAB_LINK}/${process.env.NEXT_PUBLIC_GITLAB_FRONTEND_ID}?private_token=${process.env.NEXT_PUBLIC_GITLAB_TOKEN}`,
+    )
+    const back = await fetch(
+      `${process.env.NEXT_PUBLIC_GITLAB_LINK}/${process.env.NEXT_PUBLIC_GITLAB_BACKEND_ID}?private_token=${process.env.NEXT_PUBLIC_GITLAB_TOKEN}`,
+    )
+    const [{ projects: projectsFront }, { projects: projectsBack }] =
+      await Promise.all([front.json(), back.json()])
+    projects = [...projectsFront, ...projectsBack]
+  } catch (err) {
+    console.error(err)
+  } finally {
+    return {
+      props: {
+        projects,
+      },
+    }
   }
 }
 
